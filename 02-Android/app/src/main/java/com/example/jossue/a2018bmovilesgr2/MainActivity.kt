d7301d1d844ec498b45ce02737f92f5faae9797f
jzzu <jossuedutan@gmail.com>
2018-11-11 15:50:35 -0500

    Boton de navegar (intent)

--- 02-Android/app/src/main/java/com/example/jossue/a2018bmovilesgr2/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/jossue/a2018bmovilesgr2/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.jossue.a2018bmovilesgr2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        boton_navegar.setOnClickListener {
+            //            Log.i("navegacion","Hola")
+//            Log.w("navegacion","Hola")
+//            Log.e("navegacion","Hola")
+//            Log.d("navegacion","Hola")
+//            Log.v("navegacion","Hola")
+//
+            irAPantallaDeBotones()
+        }
+
+    }
+
+    fun irAPantallaDeBotones() {
+        //INTENT
+        val intentIrABotones = Intent(this,ButtonActivity::class.java)
+        this.startActivity(intentIrABotones)
     }
 }

4cd338ea5b176213fd4738acb4c97b25d035536a
jzzu <jossuedutan@gmail.com>
2019-01-24 11:53:13 -0500

    Funciona HttpPost

--- 02-Android/app/src/main/java/com/example/jossue/a2018bmovilesgr2/HttpActivity.kt
+++ 02-Android/app/src/main/java/com/example/jossue/a2018bmovilesgr2/HttpActivity.kt
@@ -3,7 +3,9 @@ package com.example.jossue.a2018bmovilesgr2
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.beust.klaxon.Klaxon
 import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
 
 class HttpActivity : AppCompatActivity() {
@@ -14,20 +16,34 @@ class HttpActivity : AppCompatActivity() {
 
         val url = "http://172.31.105.224:1337/Estudiante"
 
+        val adrian = UsuarioHttpCreateDto("Vicente", "Eguez")
+        val adrianJson = Klaxon().toJsonString(adrian)
+        val parametros = listOf(
+            "nombre" to "Adrian",
+            "apellido" to "Eguez"
+        )
+
 
         url
-            .httpGet()
+            .httpPost(parametros)
             .responseString { request, response, result ->
 
                 when (result) {
                     is Result.Failure -> {
                         val exepcion = result.getException()
                         Log.i("http", "Error: ${exepcion}")
+                        Log.i("http", "Error: ${response}")
 
                     }
                     is Result.Success -> {
-                        val data = result.get()
-                        Log.i("http", "Datos: ${data}")
+
+                        val usuarioString = result.get()
+
+                        val usuarioClase: UsuarioHttp? = Klaxon()
+                            .parse<UsuarioHttp>(usuarioString)
+
+                        Log.i("http", "Datos: ${usuarioClase?.nombre}")
+
                     }
                 }
             }
@@ -35,3 +51,12 @@ class HttpActivity : AppCompatActivity() {
 
     }
 }
+
+class UsuarioHttp(var nombre: String,
+                  var apellido: String,
+                  var createdAt: Long? = null,
+                  var updatedAt: Long? = null,
+                  var id: Int? = null) {}
+
+class UsuarioHttpCreateDto(var nombre: String,
+                           var apellido: String) {}
